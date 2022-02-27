467bdecf5f09cbf11010ce35f10e00474e8252a2
NotReallyEight <wangzndg@gmail.com>
2022-02-27 18:31:58 +0100

    chore: general cleanup

--- src/config.ts
+++
@@ -1,8 +0,0 @@
-import { config } from "dotenv";
-
-config();
-
-export default {
-  GuildID: process.env.GUILD_ID!,
-  Token: process.env.TOKEN!,
-};
--- src/events/normalEvents/ready.ts
+++
@@ -1,5 +0,0 @@
-import { Event } from "../../utils/Event";
-
-export default new Event("ready", (client) => {
-  client.logger.info("Ready!");
-});
